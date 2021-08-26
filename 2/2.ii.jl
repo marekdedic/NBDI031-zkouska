88c9b5f66d9925515d067bae46981a34695018af
Marek Dědič <developer@dedic.eu>
2021-08-27 00:20:25 +0200

    2.ii

---
+++ 2/2.ii.jl
@@ -0,0 +1,12 @@
+using DelimitedFiles;
+
+using Plots;
+
+plotly();
+
+ds = readdlm("../data.csv", '\t', Float64);
+
+plots = map(1:size(ds, 1)) do i
+	histogram(ds[i, :], title = "Attribute " * string(i), label = "");
+end
+plot(plots..., size = (4096, 4096))
